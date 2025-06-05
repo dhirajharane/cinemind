5ac2f8e9977d939b697ed4661a8c374f90dcd2a3
dhirajharane <dhirajharane@gmail.com>
2025-06-05 14:54:56 +0530

    fixed configuration for firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,7 @@ import { GOOGLE_KEY } from "../hidden";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: {GOOGLE_KEY},
+  apiKey: GOOGLE_KEY,
   authDomain: "netflixgpt-2327e.firebaseapp.com",
   projectId: "netflixgpt-2327e",
   storageBucket: "netflixgpt-2327e.firebasestorage.app",
